c65c0cb518f6341022672ad28ebe7d7e86d26b8b
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-10-22 05:01:17 +0900

    add calendar view

--- lib/presentation/widget/reservation/form/reservation_calendar.dart
+++ lib/presentation/widget/reservation/form/reservation_calendar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_calendar/calendar.dart';
-import 'package:syncfusion_flutter_core/localizations.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class ReservationCalendar extends StatefulWidget {
   const ReservationCalendar({Key? key}) : super(key: key);
@@ -10,45 +10,167 @@ class ReservationCalendar extends StatefulWidget {
 }
 
 class _ReservationCalendarState extends State<ReservationCalendar> {
-  // DateTime firstDateOfWeekTS;
+  @override
+  Widget build(BuildContext context) {
+    print('build');
+
+    return Column(
+      children: [
+        TableCalendar(
+          focusedDay: DateTime.now(),
+          firstDay: DateTime.utc(2010, 1, 1),
+          lastDay: DateTime.utc(2030, 1, 1),
+          locale: 'ja_JP',
+          rowHeight: 70,
+          daysOfWeekHeight: 50,
+          headerStyle: CustomCalendarStyle.header(),
+          calendarBuilders: const CalendarBuilders(
+            todayBuilder: CustomCalendarBuilders.todayBuilder,
+            defaultBuilder: CustomCalendarBuilders.defaultBuilder,
+            outsideBuilder: CustomCalendarBuilders.outsideBuilder,
+            dowBuilder: CustomCalendarBuilders.daysOfWeekBuilder,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class CustomCalendarStyle {
+  static HeaderStyle header() {
+    Color textColor = Colors.white;
+
+    return HeaderStyle(
+      titleCentered: true,
+      formatButtonVisible: false,
+      headerPadding: const EdgeInsets.all(0),
+      titleTextStyle: TextStyle(
+        color: textColor,
+        fontWeight: FontWeight.bold,
+      ),
+      decoration: const BoxDecoration(
+        color: Colors.grey,
+      ),
+      leftChevronIcon: Icon(Icons.chevron_left, color: textColor),
+      rightChevronIcon: Icon(Icons.chevron_right, color: textColor),
+    );
+  }
+}
+
+class CustomCalendarBuilders {
+  static Widget todayBuilder(BuildContext context, DateTime day, DateTime focusedDay) {
+    return CustomCalendarCell(
+      dayText: day.day.toString(),
+      dayTextWeight: FontWeight.bold,
+      padding: const EdgeInsets.all(5),
+      encircle: true,
+    );
+  }
+
+  static Widget defaultBuilder(BuildContext context, DateTime day, DateTime focusedDay) {
+    return CustomCalendarCell(dayText: day.day.toString());
+  }
+
+  static Widget outsideBuilder(BuildContext context, DateTime day, DateTime focusedDay) {
+    return Container();
+  }
+
+  static Widget daysOfWeekBuilder(BuildContext context, DateTime day) {
+    final languageCode = Localizations.localeOf(context).languageCode;
+    final text =
+        const DaysOfWeekStyle().dowTextFormatter?.call(day, languageCode) ?? DateFormat.E(languageCode).format(day);
+
+    return Center(
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class CustomCalendarCell extends StatelessWidget {
+  const CustomCalendarCell({
+    Key? key,
+    required this.dayText,
+    Color? dayTextColor,
+    FontWeight? dayTextWeight,
+    EdgeInsetsGeometry? padding,
+    bool? encircle,
+  })  : dayTextColor = dayTextColor ?? Colors.black,
+        dayTextWeight = dayTextWeight ?? FontWeight.normal,
+        padding = padding ?? const EdgeInsets.all(8.0),
+        encircle = encircle ?? false,
+        super(key: key);
+
+  final String dayText;
+  final Color dayTextColor;
+  final FontWeight dayTextWeight;
+  final EdgeInsetsGeometry padding;
+  final bool encircle;
+
+  @override
+  Widget build(BuildContext context) {
+    Text text = Text(
+      dayText,
+      style: TextStyle(
+        color: dayTextColor,
+        fontWeight: dayTextWeight,
+      ),
+    );
+
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 250),
+      padding: padding,
+      decoration: BoxDecoration(border: Border.all()),
+      alignment: Alignment.topCenter,
+      child: encircle ? Encircle(text: text) : text,
+    );
+  }
+}
+
+class Encircle extends StatelessWidget {
+  const Encircle({
+    Key? key,
+    required this.text,
+    Color? bgc,
+    double? width,
+    double? height,
+    EdgeInsetsGeometry? margin,
+    EdgeInsetsGeometry? padding,
+    BoxShape? shape,
+  })  : width = width ?? 25,
+        height = height ?? 25,
+        margin = margin ?? EdgeInsets.zero,
+        padding = padding ?? const EdgeInsets.all(0),
+        shape = shape ?? BoxShape.circle,
+        bgc = bgc ?? Colors.red,
+        super(key: key);
+
+  final double width;
+  final double height;
+  final EdgeInsetsGeometry margin;
+  final EdgeInsetsGeometry padding;
+  final BoxShape shape;
+  final Color bgc;
+  final Text text;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      // color: Colors.grey,
-      child: Column(
-        children: [
-          SfCalendar(
-            view: CalendarView.week,
-            headerStyle: const CalendarHeaderStyle(
-              textAlign: TextAlign.center,
-              backgroundColor: Colors.green,
-              textStyle: TextStyle(backgroundColor: Colors.red),
-            ),
-            // todayHighlightColor: Colors.white,
-            todayTextStyle: const TextStyle(
-              color: Colors.red,
-              // backgroundColor: Colors.white,
-            ),
-            backgroundColor: Colors.grey[200],
-            headerDateFormat: 'ありがとう',
-          ),
-        ],
+      width: width,
+      height: height,
+      margin: margin,
+      padding: padding,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: bgc,
+        shape: shape,
       ),
+      child: text,
     );
   }
-
-  Widget createCalendarHeader() {
-    // 月や年が変わったりするのにも対応してないと。
-    // 月がまたがる場合もあるからな。どのように表示するのがベストだろうか
-    return Container();
-  }
-
-  Widget createCalendarBody() {
-    return Container();
-  }
-
-  // DateTime getFirstDateOfWeek(int ts) {
-
-  // }
 }
